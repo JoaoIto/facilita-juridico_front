133f572a675bda2aeb1a7b083eda929ef4d96e7c
JoaoIto <joaovictorpfr@gmail.com>
2024-03-05 16:42:13 -0300

    base add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,129 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect, useState } from 'react';
+import { ApiUtils } from "@/app/utils/api/apiMethods";
+import { tokenService } from "@/app/utils/cookies/tokenStorage";
+import Button from "@mui/material/Button";
+import * as React from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [clientes, setClientes] = useState<ICliente[]>([]); // Define o estado para armazenar os clientes
+    const [filtroNome, setFiltroNome] = useState('');
+    const [filtroEmail, setFiltroEmail] = useState('');
+    const [filtroTelefone, setFiltroTelefone] = useState('');
+
+    const fetchClientes = async (endpoint: string) => {
+        try {
+            const token = tokenService.get(); // Obtém o token de autenticação
+            console.log('Buscando clientes...');
+            const fetchedData = await ApiUtils.get<{ message: string, rows: ICliente[] }>(endpoint, token); // Faz a solicitação para obter clientes
+            console.log('Clientes encontrados:', fetchedData);
+
+            if (fetchedData && fetchedData.rows) { // Verifica se fetchedData e fetchedData.clientes são definidos
+                setClientes(fetchedData.rows); // Atualiza o estado com os clientes obtidos
+            }
+        } catch (error) {
+            console.error('Erro ao buscar clientes:', error);
+            // Trate o erro conforme necessário
+        }
+    }
+
+    useEffect(() => {
+        fetchClientes('http://localhost:8080/clientes'); // Chama a função para buscar todos os clientes quando o componente é montado
+    }, []);
+
+    const handleFetchFiltros = () => {
+        const endpoint = 'http://localhost:8080/clientes/filtro'; // Endpoint para obter clientes filtrados
+
+        // Adiciona parâmetros de filtro à URL, se houver valores nos campos de filtro
+        if (filtroNome || filtroEmail || filtroTelefone) {
+            let query = '?';
+            if (filtroNome) query += `nome=${filtroNome}&`;
+            if (filtroEmail) query += `email=${filtroEmail}&`;
+            if (filtroTelefone) query += `telefone=${filtroTelefone}&`;
+            fetchClientes(endpoint + query);
+        } else {
+            fetchClientes(endpoint);
+        }
+    };
+
+    const handleLimparFiltros = () => {
+        setFiltroNome('');
+        setFiltroEmail('');
+        setFiltroTelefone('');
+        window.location.reload();
+    };
+
+    return (
+        <div className={`h-screen w-screen flex flex-col items-center justify-center`}>
+            <h1 className={`text-center my-2 text-3xl font-medium text-start`}>Gerência de usuários</h1>
+            <h2 className={`text-2xl text-white`}>Clientes</h2>
+            <div className={`flex flex-col`}>
+                <div className={`flex w-full p-2 items-center justify-center gap-2`}>
+                    <div className={`flex flex-col`}>
+                        <label htmlFor="nome">Nome: </label>
+                        <input
+                            id={`nome`}
+                            className={`p-2 rounded border-4 border-solid border-slate-800`}
+                            placeholder={`Filtre pelo nome: `}
+                            type="text"
+                            value={filtroNome}
+                            onChange={(e) => setFiltroNome(e.target.value)}
+                        />
+                    </div>
+                    <div className={`flex flex-col`}>
+                        <label htmlFor="email">Email: </label>
+                        <input
+                            id={"email"}
+                            className={`p-2 rounded border-4 border-solid border-slate-800`}
+                            placeholder={`Filtre pelo email: `}
+                            type="text"
+                            value={filtroEmail}
+                            onChange={(e) => setFiltroEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className={`flex flex-col`}>
+                        <label htmlFor="telefone">Telefone: </label>
+                        <input
+                            id={"telefone"}
+                            className={`p-2 rounded border-4 border-solid border-slate-800`}
+                            placeholder={`Filtre pelo telefone: `}
+                            type="text"
+                            value={filtroTelefone}
+                            onChange={(e) => setFiltroTelefone(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className={`flex justify-end`}>
+                    <Button className="self-end bg-blue-900 m-2" type="submit" variant="contained" color="primary" onClick={handleLimparFiltros}>
+                        Limpar filtros
+                    </Button>
+                    <Button className="self-end bg-blue-900 m-2" type="submit" variant="contained" color="primary" onClick={handleFetchFiltros}>
+                        Filtrar
+                    </Button>
+                </div>
+            </div>
+            <table className="border-collapse border border-gray-500">
+                <thead>
+                <tr className="bg-gray-800 text-white">
+                    <th className="border border-gray-500 px-4 py-2">ID</th>
+                    <th className="border border-gray-500 px-4 py-2">Nome</th>
+                    <th className="border border-gray-500 px-4 py-2">Email</th>
+                    <th className="border border-gray-500 px-4 py-2">Telefone</th>
+                </tr>
+                </thead>
+                <tbody>
+                {clientes.map(cliente => (
+                    <tr className="bg-gray-200" key={cliente.id}>
+                        <td className="border border-gray-500 px-4 py-2">{cliente.id}</td>
+                        <td className="border border-gray-500 px-4 py-2">{cliente.nome}</td>
+                        <td className="border border-gray-500 px-4 py-2">{cliente.email}</td>
+                        <td className="border border-gray-500 px-4 py-2">{cliente.telefone}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    );
 }
